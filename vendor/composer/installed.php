300bf0ba5e42de5360853e6f339cc9da36ef268c
Mohammad Rayya <mohammad.rayya@arrabiata.de>
2024-09-23 12:35:55 +0200

    init P

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'design/design-pattern',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0e42851f8b2396b09a18e3f5915fa6b6f8b7646a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'design/design-pattern' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0e42851f8b2396b09a18e3f5915fa6b6f8b7646a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
